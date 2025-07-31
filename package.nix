d9e8aa6cd395d00aef5381b4ceff8ea65174af19
Silver <silver@squirl.dev>
2025-08-01 00:18:41 +0100

    split into multiple jobs to circumvent time limits
    i'm rly pushing the actions free plan to its absolute limits here TwT

--- package.nix
+++ package.nix
@@ -2,50 +2,25 @@
 {
   lib,
   fetchFromGitHub,
-  stdenv,
-  cmake,
-  ninja,
-  zlib,
-  python313,
-  python313Packages,
-  libedit,
-  swig,
+  mkLLVMPackages,
   enableAssertions ? true,
-  enableLibedit ? true,
-  enablePython ? true,
-}:
-stdenv.mkDerivation {
-  name = "lldb-zig-full";
-  meta = {
-    description = "A fork of LLDB, for use with the Zig self-hosted backends";
-  };
-
-  src = fetchFromGitHub {
-    owner = "jacobly0";
-    repo = "llvm-project";
-    rev = "d2c9e33092f979a2f748cef772728ad1bd45075d"; # branch lldb-zig
-    hash = "sha256-J0JUZBLrZmiJ6hidLxXS0gkelqINXrHrufoOMCn/H4U=";
-  };
-
-  nativeBuildInputs = [
-    cmake
-    ninja
-    python313
-    python313Packages.pyyaml
-  ];
-  buildInputs =
-    [zlib]
-    ++ lib.optional enableLibedit libedit
-    ++ lib.optionals enablePython [python313 swig];
-
-  cmakeBuildType = "RelWithDebInfo";
-  cmakeDir = "../llvm";
-  cmakeFlags = [
-    (lib.cmakeFeature "LLVM_ENABLE_PROJECTS" "clang;lldb")
-    (lib.cmakeBool "LLVM_ENABLE_ASSERTIONS" true)
-    (lib.cmakeBool "LLDB_ENABLE_LIBEDIT" enableLibedit)
-    (lib.cmakeBool "LLDB_ENABLE_PYTHON" enableAssertions)
-  ];
-
-  ninjaFlags = ["lldb" "lldb-server"];
-}
+  monorepoSrc ?
+    fetchFromGitHub {
+      owner = "jacobly0";
+      repo = "llvm-project";
+      rev = "d2c9e33092f979a2f748cef772728ad1bd45075d"; # branch lldb-zig
+      hash = "sha256-J0JUZBLrZmiJ6hidLxXS0gkelqINXrHrufoOMCn/H4U=";
+    },
+  llvmVersion ? "21.1.0-lldb-zig",
+}: let
+  llvmPackages =
+    (mkLLVMPackages {
+      version = llvmVersion;
+      gitRelease = {rev-version = llvmVersion;};
+      inherit monorepoSrc;
+    }).value;
+in
+  llvmPackages.lldb.override {
+    devExtraCmakeFlags = [(lib.cmakeBool "LLVM_ENABLE_ASSERTIONS" enableAssertions)];
+  }
+  // {inherit (llvmPackages) libclang libllvm;}
