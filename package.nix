83abe761e6bdf0b847ed79896d981b5a0fbfa85d
Silver <silver@squirl.dev>
2025-08-01 00:45:17 +0100

    fix version number

--- package.nix
+++ package.nix
@@ -11,7 +11,7 @@
       rev = "d2c9e33092f979a2f748cef772728ad1bd45075d"; # branch lldb-zig
       hash = "sha256-J0JUZBLrZmiJ6hidLxXS0gkelqINXrHrufoOMCn/H4U=";
     },
-  llvmVersion ? "21.1.0-lldb-zig",
+  llvmVersion ? "21.0.0-lldb-zig",
 }: let
   llvmPackages =
     (mkLLVMPackages {
